20384cf5d5a996bdfac704ce1cc7d7227a8f1d8b
Zpoxic <jjm03076@naver.com>
2021-11-04 11:07:20 +0900

    [전종민] app.py - 포스트 작성, 프로필 수정 서버 작성 완료, 좋아요 기능 0%
    index.html - Card 형식의 Post 작성 Section 분할 및 Card 생성 완료
    
    user.html - 기능 구현 50% (Post 작성 완료, 프로필 수정 완료. 프로필 로그아웃 완료
            유저의 프로필에서 유저 개인 post가 확인되지 않는 중. 좋아요 기능 구현 안됨. )
    
    myjs.js - 전체적인 순서 변경, get_posts 의 temp 작성을 Card 형식으로 변경
    
    mystyle.css - Card Section 분할을 위한 .postsection 추가 작성

--- static/myjs.js
+++ static/myjs.js
@@ -1,9 +1,58 @@
-// 좋아요
+function post() {
+    let comment = $("#textarea-post").val()
+    let today = new Date().toISOString()
+    $.ajax({
+        type: "POST",
+        url: "/posting",
+        data: {
+            comment_give: comment,
+            date_give: today
+        },
+        success: function (response) {
+            $("#modal-post").removeClass("is-active")
+            window.location.reload()
+        }
+    })
+}
+
+function time2str(date) {
+    let today = new Date()
+    let time = (today - date) / 1000 / 60  // 분
+
+    if (time < 60) {
+        return parseInt(time) + "분 전"
+    }
+    time = time / 60  // 시간
+    if (time < 24) {
+        return parseInt(time) + "시간 전"
+    }
+    time = time / 24
+    if (time < 7) {
+        return parseInt(time) + "일 전"
+    }
+    return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
+}
+
+function num2str(count) {
+    if (count > 10000) {
+        return parseInt(count / 1000) + "k"
+    }
+    if (count > 500) {
+        return parseInt(count / 100) / 10 + "k"
+    }
+    if (count == 0) {
+        return ""
+    }
+    return count
+}
+
 function toggle_like(post_id, type) {
     console.log(post_id, type)
-    let $a_like = $(`#${post_id} a[aria-label='heart']`)
+    let $a_like = $(`#${post_id} a[aria-label='${type}']`)
     let $i_like = $a_like.find("i")
-    if ($i_like.hasClass("fa-heart")) {
+    let class_s = {"heart": "fa-heart", "star": "fa-star", "like": "fa-thumbs-up"}
+    let class_o = {"heart": "fa-heart-o", "star": "fa-star-o", "like": "fa-thumbs-o-up"}
+    if ($i_like.hasClass(class_s[type])) {
         $.ajax({
             type: "POST",
             url: "/update_like",
@@ -14,7 +63,7 @@ function toggle_like(post_id, type) {
             },
             success: function (response) {
                 console.log("unlike")
-                $i_like.addClass("fa-heart-o").removeClass("fa-heart")
+                $i_like.addClass(class_o[type]).removeClass(class_s[type])
                 $a_like.find("span.like-num").text(num2str(response["count"]))
             }
         })
@@ -29,10 +78,59 @@ function toggle_like(post_id, type) {
             },
             success: function (response) {
                 console.log("like")
-                $i_like.addClass("fa-heart").removeClass("fa-heart-o")
+                $i_like.addClass(class_s[type]).removeClass(class_o[type])
                 $a_like.find("span.like-num").text(num2str(response["count"]))
             }
         })
 
     }
+}
+
+function get_posts(username) {
+    if (username == undefined) {
+        username = ""
+    }
+    $("#post-box").empty()
+    $.ajax({
+        type: "GET",
+        url: `/get_posts?username_give=${username}`,
+        data: {},
+        success: function (response) {
+            if (response["result"] == "success") {
+                let posts = response["posts"]
+                for (let i = 0; i < posts.length; i++) {
+                    let post = posts[i]
+                    let time_post = new Date(post["date"])
+                    let time_before = time2str(time_post)
+
+                    let html_temp = `<div class="card" id="${post["_id"]}" style="width: 20%; height: 20%; position: relative; float:left; margin: 5px;" >
+                                      <div class="card-image">
+                                        <figure class="image is-4by3">
+                                          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                                        </figure>
+                                      </div>
+                                      <div class="card-content">
+                                        <div class="media">
+                                          <div class="media-left">
+                                            <a class="image is-64x64" href="/user/${post['username']}">
+                                                <img class="is-rounded" src="/static/${post['profile_pic_real']}" alt="Image">
+                                            </a>
+                                          </div>
+                                          <div class="media-content">
+                                            <p class="title is-4">${post['profile_name']}</p>
+                                            <p class="subtitle is-6">@${post['username']}</p>
+                                          </div>
+                                        </div>
+                                        <div class="content">
+                                          ${post['comment']}
+                                          <br>
+                                          <time datetime="2016-1-1">${time_before}</time>
+                                        </div>
+                                      </div>
+                                    </div>`
+                    $("#post-box").append(html_temp)
+                }
+            }
+        }
+    })
 }
